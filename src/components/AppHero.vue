173ee95aecbdb4647e336548cf785956b672359c
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-12-23 15:41:44 +0100

    Completed the bonus trace

---
+++ src/components/AppHero.vue
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<template>
+    <main>
+        <section id="hero">
+            <img src="../../public/logoAOT.jpg" alt="casual img" title="casual img">
+            <p>Questo è un testo casuale per il Jumbotron. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Rerum ab dolores natus esse impedit sequi labore a quae ad mollitia. Eligendi distinctio iusto a
+                doloremque vel blanditiis quas in praesentium?</p>
+        </section>
+    </main>
+</template>
+
+<style scoped>
+
+</style>
