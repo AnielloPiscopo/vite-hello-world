cad6a3cfffe6c7b3d1d3d0984845964e5067f8a6
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-12-23 15:26:24 +0100

    Completed the main trace of the work

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
-<script setup>
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+
+export default {
+    components: {
+        AppTitle
+    }
+}
 </script>
 
 <template>
+    <AppTitle />
 </template>
 
 <style scoped>
+
 </style>
